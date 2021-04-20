b6c78b21f502d8be23247b89a8fd58b63cc91eb6
Tarek <kheirtarek@hotmail.fr>
2021-04-20 08:38:48 +0200

    task 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,34 @@
+# sets up your web servers for the deployment of web_static using puppet and add custom header
+
+package {'nginx':
+  ensure => present,
+  name   => 'nginx',
+}
+
+file {'/var/www/html/index.html':
+  ensure  => present,
+  path    => '/var/www/html/index.html',
+  content => 'Holberton School\n',
+  require => Package['nginx'],
+}
+
+file_line { 'redirect_me':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
+}
+
+file_line { 'add_header':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure     => running,
+  require    => Package['nginx'],
+}
