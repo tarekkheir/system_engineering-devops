31219d5f18b3e35b9c049b9419c7ec35cd23a27f
Tarek <kheirtarek@hotmail.fr>
2021-05-03 22:48:48 +0200

    task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" script that returns information about his/her TODO list progress"""
+
+import json
+import requests
+import sys
+
+
+def main():
+    """ main function"""
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    response = requests.get(url + sys.argv[1] + "/todos")
+    j = response.json()
+
+    r = requests.get(url + sys.argv[1])
+    jr = r.json()
+    name = jr['username']
+
+    with open("{}.json".format(sys.argv[1]), "w") as f:
+        tasks = []
+        for task in j:
+            data = {"task": task['title'], "completed": task['completed'], "username": name}
+            tasks.append(data)
+
+        file_data = {sys.argv[1]: tasks}
+        json.dump(file_data, f)
+
+
+if __name__ == "__main__":
+    main()
