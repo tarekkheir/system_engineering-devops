7b6330c8111d4600fb48fa2cbd34ca5156cd45cd
Tarek <kheirtarek@hotmail.fr>
2021-05-03 21:07:27 +0200

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -20,8 +20,7 @@ def main():
     with open("{}.csv".format(sys.argv[1]), "w") as f:
         writer = csv.writer(f, quoting=csv.QUOTE_MINIMAL)
         for task in json:
-            status = str(task.get('completed'))
-            row = [sys.argv[1], name, status, task.get('title')]
+            row = [task.get('userId'), name, task.get('completed'), task.get('title')]
             writer.writerow(row)
 
 
