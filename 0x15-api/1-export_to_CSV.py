97a1e8863cd26953d767f4ce242e33567a4cd57f
Tarek <kheirtarek@hotmail.fr>
2021-05-03 21:04:39 +0200

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" script that returns information about his/her TODO list progress"""
+
+
+import sys
+import requests
+import csv
+
+
+def main():
+    """ main function"""
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    response = requests.get(url + sys.argv[1] + "/todos")
+    json = response.json()
+
+    r = requests.get(url + sys.argv[1])
+    j = r.json()
+    name = j['name']
+
+    with open("{}.csv".format(sys.argv[1]), "w") as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_MINIMAL)
+        for task in json:
+            status = str(task.get('completed'))
+            row = [sys.argv[1], name, status, task.get('title')]
+            writer.writerow(row)
+
+
+if __name__ == "__main__":
+    main()
