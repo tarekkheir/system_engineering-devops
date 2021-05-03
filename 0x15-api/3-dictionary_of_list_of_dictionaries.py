a3d769b3bfc6d1e42892882ed630e86cc9212adc
Tarek <kheirtarek@hotmail.fr>
2021-05-03 23:34:23 +0200

    task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" script that returns information about his/her TODO list progress"""
+
+import json
+import requests
+
+
+def main():
+    """ main function"""
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    response = requests.get(url)
+    j = response.json()
+
+    all_users_data = {}
+
+    for user in j:
+        r = requests.get(url + "/todos" + str(user['id'])).json()
+        tasks = []
+
+        for task in r:
+            data = {"task": task['title'], "completed": task['completed'],
+                    "username": name}
+            tasks.append(data)
+            all_users_data[user('id')] = tasks
+
+    with open("todo_all_employees.json", "w") as f:
+        json.dump(all_users_data, f)
+
+
+if __name__ == "__main__":
+    main()
