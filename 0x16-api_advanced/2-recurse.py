9faceb0af54e970929f40e5abe1dc2146992bf04
Tarek <kheirtarek@hotmail.fr>
2021-05-18 14:35:05 +0200

    task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,7 +4,7 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=None):
     """ returns a list containing the titles of all
     hot articles for a given subreddit"""
     url = 'https://www.reddit.com/r/' + subreddit + '/hot/.json'
@@ -16,7 +16,11 @@ def recurse(subreddit, hot_list=[]):
     if response.status_code == 404:
         return None
     else:
+        after = response.json()['data']['after']
         children = response.json()['data']['children']
         for title in children:
             hot_list.append(title)
-        return recurse(subreddit, hot_list)
+        if after is None:
+            return hot_list
+
+    return recurse(subreddit, hot_list, after)
