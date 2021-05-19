4a4038458a6175c7544e24d847ef53f2e60207bf
Tarek <kheirtarek@hotmail.fr>
2021-05-19 22:11:07 +0200

    task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Fix settings by adding line in wp-settings.php
+# Fix Wordpress file
 exec { 'correction': 
   command => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/" /var/www/html/wp-settings.php'
   path => '/usr/bin/:/bin:usr/bin/env'
