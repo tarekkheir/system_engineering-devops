f76ce19d3b2651f2d49be5a96b709c94cbec5e0c
Tarek <kheirtarek@hotmail.fr>
2021-05-19 22:06:37 +0200

    task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fix settings by adding line in wp-settings.php
 exec { 'correction': 
-  command => 'sed -i "/phpp/php/g" /var/www/html/wp-settings.php'
+  command => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/" /var/www/html/wp-settings.php'
   path => '/usr/bin/:/bin:usr/bin/env'
 }
